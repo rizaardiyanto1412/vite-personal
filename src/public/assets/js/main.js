f1b71a265069205b67f3e2878699194b76ea8fba
Riza Ardiyanto <rizaardiyanto@Rizas-MacBook-Air.local>
2024-01-20 22:44:27 +0700

    fix everything

--- src/public/assets/js/main.js
+++ src/public/assets/js/main.js
@@ -1,11 +1,19 @@
+// Importing necessary libraries
 import confetti from 'canvas-confetti';
+import Typed from 'typed.js';
 
+// Getting the button element
 const button = document.getElementById('btn-convetti');
 
+// Adding event listener to the button
 button.addEventListener('click', () => {
-  console.log("Hello");
+  // Creating a new canvas element
   const myCanvas = document.createElement('canvas');
+
+  // Appending the canvas to the body
   document.body.append(myCanvas);
+
+  // Setting the canvas style
   myCanvas.style.position = "fixed";
   myCanvas.style.top = "0"; 
   myCanvas.style.left = "0"; 
@@ -13,27 +21,23 @@ button.addEventListener('click', () => {
   myCanvas.style.height = "100vh";
   myCanvas.style.zIndex = -1;
 
+  // Creating a confetti instance
   var myConfetti = confetti.create(myCanvas, {
     resize: true,
     useWorker: true
   });
   
+  // Firing the confetti
   myConfetti({
     particleCount: 100,
     spread: 160
-    
   });
-
-  
 });
 
-
-
-// Typed.js
-import Typed from 'typed.js';
-
+// Getting the title element
 const title = document.getElementById('title');
 
+// Creating a new Typed instance
 const type = new Typed('#title', {
   strings: [
     'Riza Maulana',
@@ -43,7 +47,5 @@ const type = new Typed('#title', {
   ],
   typeSpeed: 50,
   loop: true,
-  loop: Infinity,
   showCursor: false,
-
 });
